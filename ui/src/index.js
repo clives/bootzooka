3968548734e6b58834fcb4715abd06eef829473c
sosthene <Clive.St@protonmail.com>
2019-09-12 15:20:59 -0300

    Footer unit test fixed

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,3 +1,4 @@
+import { hot } from 'react-hot-loader/root'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,22 +7,70 @@ import App from './App';
 import PasswordService from './PasswordService/PasswordService';
 import UserService from './UserService/UserService';
 import VersionService from './VersionService/VersionService';
-import reducer from './reducers'
-import rootSaga from './sagas'
-import { logger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './Reducers/Reducers'
 import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
-
-const passwordService = new PasswordService();
+import registerServiceWorker from './registerServiceWorker.js';
+//import configureStore from './store';
+import App2 from './App2'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { logger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './Sagas/Sagas'
+import { AppContainer } from 'react-hot-loader';
+//import makeStore from './configureStore';
 
 const sagaMiddleware = createSagaMiddleware()
+//const store = configureStore();
+
+//const store = makeStore();
+
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleware, logger)))
 
 sagaMiddleware.run(rootSaga)
 
+//registerServiceWorker();
+
+/*
+const render = Component => {
+  return ReactDOM.render(
+    <Provider store={store}>
+          <BrowserRouter>
+            <Component/>
+            </BrowserRouter>
+          </Provider>,
+          document.getElementById('root'))
+}*/
+
+/*
+render(App);
+
+
+console.log("module.hot???");
+if (module.hot) {
+  console.log("module.hot");
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
+*/
+//export default process.env.NODE_ENV === "development" ? hot(module)(render(App)) : render(App)
+
+
+/*
+const render = Component => {
+  return ReactDOM.render( <Component/>,document.getElementById('root'))
+}
+*/
+/*
+const App2 = () => <div>Hello World!</div>
+
+export default process.env.NODE_ENV === "development" ? hot(App2) : render(App2)
+*/
+
+/*
 ReactDOM.render(
  <Provider store={store}>
   <BrowserRouter>
@@ -32,3 +81,62 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+*/
+
+
+
+
+// !AppContainer should wrapp only one component!!!
+const renderApp2 = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Component/>
+          </BrowserRouter>
+        </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(App);
+
+/*
+module.hot.accept('./App.js', () => {
+     const NextRootContainer = require('./App.js').default;
+     <Provider store={store}>
+        <BrowserRouter>
+          <App/>
+          </BrowserRouter>
+        </Provider>
+       , document.getElementById('root');
+   })*/
+
+
+if (module.hot) {
+  module.hot.accept('./App.js', () => {
+    console.log("reload App.js")
+
+    const NextRootContainer = require('./App.js').default;
+    renderApp2(NextRootContainer);
+  });
+}
+
+
+/*
+ReactDOM.render(
+ <App2/>,
+  document.getElementById('root')
+);
+*/
+
