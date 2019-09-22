d0090ac3cb2a8d5bd5eacb068eb77236da806acc
sosthene <Clive.St@protonmail.com>
2019-09-22 12:48:47 -0300

    google sign in - adding action to update the backend with google id

--- ui/src/Register/Google_signin.js
+++ ui/src/Register/Google_signin.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {userLoggedIn, userLoggedInWithGoogle} from '../Actions/Actions';
 
 const GOOGLE_BUTTON_ID = 'google-sign-in-button';
+const AUTH_SERVICE_NAME= 'google'
 
 class SignIn extends Component {
 
@@ -25,23 +28,36 @@ class SignIn extends Component {
         
     }*/
 
-    onSuccess() {
-      this.setState({
-        isSignedIn: true
-      })
+    onSuccess(googleUser) {
+      console.log("onSuccess")
+      console.log("basicProfile:"+googleUser.getBasicProfile());
+      this.props.userLoggedIn(AUTH_SERVICE_NAME)      
     }
 
     //src: https://www.intricatecloud.io/2019/08/adding-google-sign-in-to-your-webapp-a-react-example/
     componentDidMount() {
       window.gapi.load('auth2', () => {
         this.auth2 = window.gapi.auth2.init({
-          client_id: 'XXX.googleusercontent.com',
+          client_id: '916742357497-o99psqoc143vsqi93udn95e4c0r6o26a.apps.googleusercontent.com',
         })
 
-        this.auth2.then(() => {
-          this.setState({
-            isSignedIn: this.auth2.isSignedIn.get(),
-          });
+        this.auth2.then((token) => {
+          console.log("token google:"+token)
+
+          //https://developers.google.com/identity/sign-in/web/reference
+          const googleUser = window.gapi.auth2.getAuthInstance().currentUser.get()
+
+          
+
+          if( this.auth2.isSignedIn.get() ){
+
+            const profile = googleUser.getBasicProfile();
+            const id_token = googleUser.getAuthResponse().id_token;
+            const uniqueGogoleId = googleUser.getId()
+
+            this.props.userLoggedInWithGoogle(uniqueGogoleId)
+          }
+           // this.props.userLoggedIn(AUTH_SERVICE_NAME);
         });
     
         var opts = {
@@ -62,5 +78,13 @@ class SignIn extends Component {
 render = () => (<div id={GOOGLE_BUTTON_ID}> google </div>)
 }
 
-export default SignIn;
+export const mapStateToProps = (state) => ({  
+})
+
+const mapDispatchToProps = {
+  userLoggedIn: userLoggedIn,
+  userLoggedInWithGoogle: userLoggedInWithGoogle
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
 
